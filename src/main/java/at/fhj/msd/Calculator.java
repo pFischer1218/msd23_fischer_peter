d07328afc79964abd69a003b689b6a3132aef688
pFischer1218 <peter.fischer@edu.fh-joanneum.at>
2024-07-03 21:32:13 +0200

    Added Javadoc comments to the Calculator class Added descriptions to the class, the methodes and parameters and added return values and exceptions.

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,6 +1,7 @@
 package at.fhj.msd;
 
 import org.apache.logging.log4j.LogManager;
+
 import org.apache.logging.log4j.Logger;
 
 /**
@@ -8,7 +9,6 @@ import org.apache.logging.log4j.Logger;
  * It logs each operation at the debug level using Log4j.
  */
 public class Calculator {
-
     private static Logger logger = LogManager.getLogger();
 
     /**
@@ -19,7 +19,9 @@ public class Calculator {
      * @return the sum of number1 and number2
      */
     public double add(double number1, double number2) {
+
         logger.debug("addiert: {} und {}", number1, number2);
+
         return number1 + number2;
     }
 
@@ -31,7 +33,9 @@ public class Calculator {
      * @return the result of subtracting number2 from number1
      */
     public double minus(double number1, double number2) {
+
         logger.debug("subtrahiert: {} und {}", number1, number2);
+
         return number1 - number2;
     }
 
@@ -43,7 +47,9 @@ public class Calculator {
      * @return the result of multiplying number1 and number2
      */
     public double multiply(double number1, double number2) {
+
         logger.debug("multipliziert: {} und {}", number1, number2);
+
         return number1 * number2;
     }
 
@@ -56,11 +62,12 @@ public class Calculator {
      * @throws ArithmeticException if number2 is zero
      */
     public double divide(double number1, double number2) {
+
         logger.debug("dividiert: {} und {}", number1, number2);
 
         if (number2 == 0) {
             logger.error("Nulldivision nicht moeglich");
-            throw new ArithmeticException("Division by zero is not allowed");
+            throw new ArithmeticException();
         }
 
         return number1 / number2;
@@ -73,13 +80,14 @@ public class Calculator {
      * @return the factorial of the number, or 0 if the number is negative
      */
     public int factorial(int number) {
+
+
         if (number < 0) {
             return 0;
         }
 
-        if (number >= 1) {
+        if (number >= 1)
             return number * factorial(number - 1);
-        }
 
         return 1;
     }
